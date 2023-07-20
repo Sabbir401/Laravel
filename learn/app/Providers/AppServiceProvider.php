8b4b53bd9993379c1867747b665dded3ab8a4851
Kazi Sabbir Ahmed Opi <ahmedsabbir401@gmail.com>
2023-07-21 02:05:05 +0600

    21 july

--- learn/app/Providers/AppServiceProvider.php
+++ learn/app/Providers/AppServiceProvider.php
@@ -11,7 +11,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind('first_service_helper', function($app){
+            dd("this is my first service provider");
+        });
+
+        app()->bind('second_service_helper', function($app){
+            dd("this is my second service provider");
+        });
+
+        app()->bind('third_service_helper', function($app){
+            dd("this is my third service provider");
+        });
     }
 
     /**
--- learn/routes/web.php
+++ learn/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(app());
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    app()->make('first_service_helper');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
